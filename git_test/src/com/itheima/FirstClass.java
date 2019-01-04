d1cf0d4461904e86e213157f49259359944cc929
zhangsan <zhangsan@com.itheima>
2019-01-04 16:44:00 +0800

    修改

---
+++ git_test/src/com/itheima/FirstClass.java
@@ -0,0 +1,16 @@
+package com.itheima;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+    }
+
+    public void test(){
+        int i=0;
+        i+=10;
+    }
+}
